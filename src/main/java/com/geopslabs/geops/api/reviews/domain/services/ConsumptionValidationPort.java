63a3e6accdc68ed79473b69379149c7bda9cc00b
Bryan Eduardo Barba Estrada <u202323479@upc.edu.pe>
2026-05-05 13:53:58 -0500

    refactor: remove reviews feature

--- src/main/java/com/geopslabs/geops/api/reviews/domain/services/ConsumptionValidationPort.java
+++
@@ -1,5 +0,0 @@
-package com.geopslabs.geops.api.reviews.domain.services;
-
-public interface ConsumptionValidationPort {
-    boolean existsByUserAndOffer(Long userId, Long offerId);
-}
--- src/main/java/com/geopslabs/geops/api/reviews/domain/services/UserValidationPort.java
+++
@@ -1,5 +0,0 @@
-package com.geopslabs.geops.api.reviews.domain.services;
-
-public interface UserValidationPort {
-    boolean existsById(Long userId);
-}
